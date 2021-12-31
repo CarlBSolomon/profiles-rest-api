c013f049586b17bf607d4e8b5da0aef1f00617c0
CarlBSolomon <36128790+CarlBSolomon@users.noreply.github.com>
2021-12-31 14:40:39 +0000

    corrected a serializer reference due to missing s - Tenth Commit

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 
 class Helloserializer(serializers.Serializer):
     """ Serialises a name field for testing our APIView"""
-    name = Serializer.CharField(max_length=10)
+    name = serializers.CharField(max_length=10)
