ced4b56642389008513488d2b42783a2cb6757bf
CarlBSolomon <36128790+CarlBSolomon@users.noreply.github.com>
2022-01-02 10:26:32 +0000

    Added put, patch and delete functionality to views - Eleventh Commit

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -8,7 +8,7 @@ from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
-    serializers_class = serializers.Helloserializer
+    serializer_class = serializers.Helloserializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -24,7 +24,7 @@ class HelloApiView(APIView):
 
     def post(self, request):
         """Create a hello mesage with our name"""
-        serializer = self.serializers_class(data=request.data)
+        serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
@@ -32,3 +32,15 @@ class HelloApiView(APIView):
             return Response({'message': message})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """ Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
